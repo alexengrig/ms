2bb498583f6359afcf99f3c1778d45f79b355a9e
Grig Alex <alexengrigdev@gmail.com>
2021-09-26 13:20:07 +0300

    Create SlothController

---
+++ sloth-rest/src/main/java/com/example/ms/sloth/rest/controller/SlothController.java
@@ -0,0 +1,30 @@
+package com.example.ms.sloth.rest.controller;
+
+import com.example.ms.sloth.rest.service.SlothService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sloths")
+@RequiredArgsConstructor
+public class SlothController {
+
+    private final SlothService service;
+
+    @GetMapping
+    public ResponseEntity<?> all() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PostMapping("/{id}/sleep")
+    public ResponseEntity<?> sleep(@PathVariable String id, @RequestParam("timeout") long timeout) {
+        return ResponseEntity.ok(service.sleep(id, timeout));
+    }
+
+}
