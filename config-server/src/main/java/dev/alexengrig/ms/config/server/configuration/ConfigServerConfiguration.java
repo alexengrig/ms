a39b1af83ed0230ea9a7c54eea0abfda54dd7a15
Grig Alex <alexengrigdev@gmail.com>
2021-09-27 23:37:11 +0300

    Create config-server

---
+++ config-server/src/main/java/dev/alexengrig/ms/config/server/configuration/ConfigServerConfiguration.java
@@ -0,0 +1,9 @@
+package dev.alexengrig.ms.config.server.configuration;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfiguration {
+}
